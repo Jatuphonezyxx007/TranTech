967cbbe703985b166be643e48cef40feeacb5853
Jatuphon Wongputta <144703186+Jatuphonezyxx007@users.noreply.github.com>
2024-04-17 15:34:41 +0700

    upload project
    Business Programming subject booking ticket code c#

---
+++ TransTechApp/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TransTechApp
+{
+    public partial class LoginPage : Form
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUsername.Text))
+            {
+                MessageBox.Show("Please enter your Username.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUsername.Focus();
+                return;
+            }
+            try
+            {
+                TransTechAppDataSetUsernamePasswordTableAdapters.employeeTableAdapter user = new TransTechAppDataSetUsernamePasswordTableAdapters.employeeTableAdapter();
+                TransTechAppDataSetUsernamePassword.employeeDataTable dt = user.GetDataByUsernamePassword(txtUsername.Text, txtPassword.Text);
+                if(dt.Rows.Count > 0)
+                {
+                    //MessageBox.Show("You have been successfully logged in.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Form2 frm = new Form2();
+                    frm.ShowDialog();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Your username or password is incorrect.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtPassword.Clear();
+                    txtUsername.Clear();
+                    txtUsername.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult exit;
+            exit = MessageBox.Show("Are you sure?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (exit == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void ckShowPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckShowPass.Checked)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void LoginPage_Load(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = true;
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TransTechApp/Form2.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace TransTechApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.แฟมขอมลToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.การจองตวToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ขอมลพนกงานToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.แฟมขอมลToolStripMenuItem,
+            this.การจองตวToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // แฟมขอมลToolStripMenuItem
+            // 
+            this.แฟมขอมลToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ขอมลพนกงานToolStripMenuItem});
+            this.แฟมขอมลToolStripMenuItem.Name = "แฟมขอมลToolStripMenuItem";
+            this.แฟมขอมลToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.แฟมขอมลToolStripMenuItem.Text = "แฟ้มข้อมูล";
+            // 
+            // การจองตวToolStripMenuItem
+            // 
+            this.การจองตวToolStripMenuItem.Name = "การจองตวToolStripMenuItem";
+            this.การจองตวToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.การจองตวToolStripMenuItem.Text = "การจองตั๋ว";
+            // 
+            // ขอมลพนกงานToolStripMenuItem
+            // 
+            this.ขอมลพนกงานToolStripMenuItem.Name = "ขอมลพนกงานToolStripMenuItem";
+            this.ขอมลพนกงานToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ขอมลพนกงานToolStripMenuItem.Text = "ข้อมูลพนักงาน";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 522);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem แฟมขอมลToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem การจองตวToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ขอมลพนกงานToolStripMenuItem;
+    }
+}
